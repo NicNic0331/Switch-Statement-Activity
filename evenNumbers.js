dec79bc3488c8deb9354d15ae78679b81e61f628
NicNic0331 <princessnicolesalazar31@gmail.com>
2024-10-01 13:05:30 +0800

    Delete evenNumbers.js

--- evenNumbers.js
+++
@@ -1,7 +0,0 @@
-// Print numbers from 1 to 10 using a for loop
-for (let i = 1; i <= 10; i++) {
-    // Modify to print only even numbers between 1 and 10
-    if (i % 2 === 0) {
-      console.log(i);
-    }
-  }
